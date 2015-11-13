6542b2c7da70d9b3373a4f5ab67a6bb1e25aa4da
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-11-12 22:32:02 -0500

    Fixed bug where if you click on the friends list view in friendsactivity the app crashes

--- app/src/main/java/com/example/nikhil/myapplication/FriendsActivity.java
+++ app/src/main/java/com/example/nikhil/myapplication/FriendsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -93,6 +95,12 @@ public class FriendsActivity extends ActionBarActivity {
                 android.R.layout.simple_list_item_1, friends);
 
         friendsListView.setAdapter(adapter);
+        friendsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(FriendsActivity.this, friends.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         // CONNECT TO SERVER
